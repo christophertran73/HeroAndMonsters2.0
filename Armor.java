a00f510df7f46901b62da3dec5a1fafc126dac35
christophertran73 <35276390+christophertran73@users.noreply.github.com>
2018-02-05 23:49:50 -0800

    backspace

--- Armor.java
+++ Armor.java
@@ -5,8 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Armor
-{
+public class Armor{
     // instance variables - replace the example below with your own
     private int x;
 
